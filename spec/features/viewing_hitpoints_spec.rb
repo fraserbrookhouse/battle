9117924d25f0ba1a9a5a1517cace7333d762c1f5
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-18 15:36:42 +0100

    aded web_helper.rb, extracted sign in/name entry to a method to avoid reptition in tests

--- spec/features/viewing_hitpoints_spec.rb
+++ spec/features/viewing_hitpoints_spec.rb
@@ -1,9 +1,6 @@
 feature 'viewing hitpoints' do
   scenario 'able to see player hitpoints on page' do
-    visit('/')
-    fill_in :player_1_name, with: 'Voldemort'
-    fill_in :player_2_name, with: 'Thanos'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Voldemort: 100hp'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'rspec'
 require 'capybara'
 require 'capybara/rspec'
+require 'features/web_helpers'
 
 Capybara.app = Battle
 
