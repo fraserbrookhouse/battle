e1a3ae28ee1c9510fa4e3550829dbffbd97c3ff0
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-19 14:09:43 +0100

    added failing test for attack class that I will extract

---
+++ spec/attack_spec.rb
@@ -0,0 +1,12 @@
+require 'attack'
+
+describe Attack do
+  let(:player) { double :player}
+
+  describe '#punch' do
+    it 'damages the player' do
+      expect(player).to receive(:receive_damage)
+      subject.punch(player)
+    end
+  end
+end
