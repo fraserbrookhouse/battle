c399e7d99b7af286fc02e09c619ab2532a2ddf57
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-20 17:27:22 +0100

    used srand to pass capybara tests that relied on random number generation, passes all tests

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -5,21 +5,18 @@ feature 'Attacking' do
     expect(page).to have_content 'Voldemort attacked Thanos'
   end
 
-  scenario 'reduce player 2 health to 90' do
+  scenario 'reduce player 2 health to 92' do
+    srand(777)
     sign_in_and_play
     click_button 'Attack'
-    expect(page).to have_content 'Thanos: 90HP'
+    expect(page).to have_content 'Thanos: 92HP'
   end
 
-  scenario 'reduce Player 1 health to 90' do
+  scenario 'reduce Player 1 health to 84' do
+    srand(777)
     sign_in_and_play
     attack_and_confirm
     click_button 'Attack'
-    expect(page).to have_content 'Voldemort: 90HP'
-  end
-  scenario 'reduce player 2 health to 90' do
-    sign_in_and_play
-    click_button 'Attack'
-    expect(page).to have_content 'Thanos: 90HP'
+    expect(page).to have_content 'Voldemort: 84HP'
   end
 end
--- spec/features/game_over_spec.rb
+++ spec/features/game_over_spec.rb
@@ -1,8 +1,9 @@
 feature 'Game over' do
   context 'when Player 2 reaches 0 HP first' do
     before do
+      srand(777)
       sign_in_and_play
-      18.times { attack_and_confirm }
+      10.times { attack_and_confirm }
     end
     scenario 'Player 1 loses' do
       click_button 'Attack'
